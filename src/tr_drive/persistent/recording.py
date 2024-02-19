af612bacba4630de3c92c7df62f358e18c474d0f
gralerfics <glverfer@outlook.com>
2024-02-20 01:01:21 +0800

    feat(recording): support ground truth data saving and loading. (not tested)

--- src/tr_drive/persistent/recording.py
+++ src/tr_drive/persistent/recording.py
@@ -25,7 +25,8 @@ class Recording:
             'folders': {
                 'raw_image': '/raw_image',
                 'processed_image': '/processed_image',
-                'odom': '/odom'
+                'odom': '/odom',
+                'ground_truth': '/ground_truth'
             },
             'image': {
                 'raw_size': None, # [width, height]
@@ -42,11 +43,12 @@ class Recording:
         self.raw_images: list[DigitalImage] = []
         self.processed_images: list[DigitalImage] = []
         self.odoms: list[Frame] = []
+        self.ground_truths: list[Frame] = []
         
         self.ZERO_FILL_LENGTH = 6 # temporary
     
     @staticmethod
-    def from_path(path): # with name, TODO: index check
+    def from_path(path): # with name, TODO: index check; exists check
         recording = Recording()
         with open(path + '/parameters.json', 'r') as f:
             recording.params = json.load(f)
@@ -54,6 +56,8 @@ class Recording:
         
         # load processed images
         processed_image_folder = path + recording.params['folders']['processed_image']
+        if not os.path.exists(processed_image_folder):
+            raise Exception('Processed image data does not exist.')
         for filename in sorted(os.listdir(processed_image_folder)):
             if filename.endswith('.jpg'):
                 img_cv2 = cv2.imread(processed_image_folder + '/' + filename, cv2.IMREAD_GRAYSCALE)
@@ -65,11 +69,22 @@ class Recording:
         
         # load odometry
         odom_folder = path + recording.params['folders']['odom']
+        if not os.path.exists(odom_folder):
+            raise Exception('Odom data does not exist.')
         for filename in sorted(os.listdir(odom_folder)):
             if filename.endswith('.json'):
                 with open(odom_folder + '/' + filename, 'r') as f:
                     odom_dict = json.load(f)
                     recording.odoms.append(Frame.from_dict(odom_dict))
+        
+        # load ground truth (optional)
+        ground_truth_folder = path + recording.params['folders']['ground_truth']
+        if os.path.exists(ground_truth_folder):
+            for filename in sorted(os.listdir(ground_truth_folder)):
+                if filename.endswith('.json'):
+                    with open(ground_truth_folder + '/' + filename, 'r') as f:
+                        ground_truth_dict = json.load(f)
+                        recording.ground_truths.append(Frame.from_dict(ground_truth_dict))
         
         # load raw images and reprocess if validation failed
         if not valid:
@@ -77,6 +92,9 @@ class Recording:
             recording.processed_images.clear()
             
             raw_image_folder = path + recording.params['folders']['raw_image']
+            if not os.path.exists(raw_image_folder):
+                raise Exception('Raw image data does not exist.')
+            
             resize = recording.params['image']['resize']
             patch_size = recording.params['image']['patch_size']
             
@@ -105,28 +123,36 @@ class Recording:
             json.dump(self.params, f)
         
         # save raw images
-        if self.params['folders']['raw_image'] is not None:
-            if self.params['folders']['raw_image'] is not None:
-                raw_image_folder = path + self.params['folders']['raw_image']
-                os.makedirs(raw_image_folder, exist_ok = True)
-                for i, img in enumerate(self.raw_images):
-                    img.to_jpg(raw_image_folder + '/' + str(i).zfill(self.ZERO_FILL_LENGTH) + '.jpg')
+        if len(self.raw_images) > 0:
+            raw_image_folder = path + self.params['folders']['raw_image']
+            os.makedirs(raw_image_folder, exist_ok = True)
+            for i, img in enumerate(self.raw_images):
+                img.to_jpg(raw_image_folder + '/' + str(i).zfill(self.ZERO_FILL_LENGTH) + '.jpg')
         
         # save processed images
+        if len(self.processed_images) < 0:
+            raise Exception('No processed image data to save.')
         processed_image_folder = path + self.params['folders']['processed_image']
         os.makedirs(processed_image_folder, exist_ok = True)
         for i, img in enumerate(self.processed_images):
             img.to_jpg(processed_image_folder + '/' + str(i).zfill(self.ZERO_FILL_LENGTH) + '.jpg')
         
         # save odometry
+        if len(self.odoms) < 0:
+            raise Exception('No odometry data to save.')
         odom_folder = path + self.params['folders']['odom']
         os.makedirs(odom_folder, exist_ok = True)
         for i, odom in enumerate(self.odoms):
             with open(odom_folder + '/' + str(i).zfill(self.ZERO_FILL_LENGTH) + '.json', 'w') as f:
                 json.dump(odom.to_dict(), f)
-    
-    def set_raw_image_folder(self, folder): # pass None to disable
-        self.params['folders']['raw_image'] = folder
+        
+        # save ground truth (optional)
+        if len(self.ground_truths) > 0:
+            ground_truth_folder = path + self.params['folders']['ground_truth']
+            os.makedirs(ground_truth_folder, exist_ok = True)
+            for i, ground_truth in enumerate(self.ground_truths):
+                with open(ground_truth_folder + '/' + str(i).zfill(self.ZERO_FILL_LENGTH) + '.json', 'w') as f:
+                    json.dump(ground_truth.to_dict(), f)
     
     def set_image_parameters(self, raw_size, patch_size, resize, horizontal_fov):
         self.params['image'] = {
