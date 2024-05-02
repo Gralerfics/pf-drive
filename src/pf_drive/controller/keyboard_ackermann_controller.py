d9a5f79042d440ea0ce0bf0e8bb52ceb45e623c8
gralerfics <glverfer@outlook.com>
2024-05-02 14:05:25 +0800

    feat(ackermann): lower center of mass for the robot model; optimize keyboard_ackermann_controller.

--- src/pf_drive/controller/keyboard_ackermann_controller.py
+++ src/pf_drive/controller/keyboard_ackermann_controller.py
@@ -18,6 +18,7 @@ class KeyboardAckermannController(Node):
         decay_v = 0.3
         max_phi = 0.48
         delta_phi = 0.3
+        decay_phi = 0.3
 
         v = 0.0
         phi = 0.0
@@ -26,9 +27,11 @@ class KeyboardAckermannController(Node):
         down_pressed = False
         left_pressed = False
         right_pressed = False
+        z_pressed = False # 回正
+        space_pressed = False # 手刹
 
         def on_press(key):
-            nonlocal up_pressed, down_pressed, left_pressed, right_pressed
+            nonlocal up_pressed, down_pressed, left_pressed, right_pressed, z_pressed, space_pressed
             if key == keyboard.Key.up:
                 up_pressed = True
             elif key == keyboard.Key.down:
@@ -37,9 +40,13 @@ class KeyboardAckermannController(Node):
                 left_pressed = True
             elif key == keyboard.Key.right:
                 right_pressed = True
+            elif key == keyboard.KeyCode.from_char('z'):
+                z_pressed = True
+            elif key == keyboard.Key.space:
+                space_pressed = True
         
         def on_release(key):
-            nonlocal up_pressed, down_pressed, left_pressed, right_pressed
+            nonlocal up_pressed, down_pressed, left_pressed, right_pressed, z_pressed, space_pressed
             if key == keyboard.Key.up:
                 up_pressed = False
             elif key == keyboard.Key.down:
@@ -48,6 +55,10 @@ class KeyboardAckermannController(Node):
                 left_pressed = False
             elif key == keyboard.Key.right:
                 right_pressed = False
+            elif key == keyboard.KeyCode.from_char('z'):
+                z_pressed = False
+            elif key == keyboard.Key.space:
+                space_pressed = False
 
         with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
             last_t = time.time()
@@ -70,17 +81,24 @@ class KeyboardAckermannController(Node):
                         v = max(-max_v, v - delta_v * dt)
 
                 if left_pressed == right_pressed:
-                    phi = 0
+                    # phi = 0
+                    phi = phi * (1 - decay_phi * dt)
                 if left_pressed and not right_pressed:
-                    if phi < 0:
-                        phi = 0
-                    else:
-                        phi = min(max_phi, phi + delta_phi * dt)
+                    # if phi < 0:
+                    #     phi = 0
+                    # else:
+                    phi = min(max_phi, phi + delta_phi * dt)
                 if right_pressed and not left_pressed:
-                    if phi > 0:
-                        phi = 0
-                    else:
-                        phi = max(-max_phi, phi - delta_phi * dt)
+                    # if phi > 0:
+                    #     phi = 0
+                    # else:
+                    phi = max(-max_phi, phi - delta_phi * dt)
+                
+                if z_pressed:
+                    phi = 0
+                
+                if space_pressed:
+                    v = 0
 
                 self.io['actuator_command'].write(('vphi', v, phi))
                 time.sleep(0.05)
