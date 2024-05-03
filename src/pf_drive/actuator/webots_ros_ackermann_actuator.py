feat(ackermann): separate computation and actuation to improve the performance; fix the problem that odom calculation depended on the command frequency.

src/pf_drive/actuator/webots_ros_ackermann_actuator.py
@@ -49,18 +49,17 @@ class WebotsRotationalMotorController:
         format:
             (type = 'vw', v, w)
             (type = 'vphi', v, phi)
-    `odom`, output (any) [optional]
+    `odom`, output (any)
         format:
             4x4 np.array, TODO: 是否 .tolist() 减少少量长度? 需与转换耗时比较.
+    `param`, output (any)
+        format:
+            [phi_l, phi_r, w_rear]
     Notes:
-        1. Cable 建议使用 latest = True 的 pipe.
+        Cable 建议使用 latest = True 的 pipe.
 """
-class WebotsROSAckermannActuator(Node):
+class WebotsROSAckermannActuatorComputer(Node):
     def __init__(self, name,
-        left_front_steer_motor_srv,
-        right_front_steer_motor_srv,
-        left_rear_motor_srv,
-        right_rear_motor_srv,
         get_time_srv,
         track,
         wheelbase,
@@ -68,11 +67,7 @@ class WebotsROSAckermannActuator(Node):
         max_steering_angle
     ):
         super().__init__(name)
-        
-        self.left_front_steer_motor_srv = left_front_steer_motor_srv
-        self.right_front_steer_motor_srv = right_front_steer_motor_srv
-        self.left_rear_motor_srv = left_rear_motor_srv
-        self.right_rear_motor_srv = right_rear_motor_srv
+
         self.get_time_srv = get_time_srv
 
         self.l = track
@@ -83,23 +78,15 @@ class WebotsROSAckermannActuator(Node):
         self.R_min_abs = self.d / np.tan(self.max_phi) + self.l / 2
 
         self.odom = np.eye(4)
+        self.v_last = 0
+        self.R_last = 1e9
 
         self.ros = ROSContext(self.name)
-        self.left_front_steer_motor = WebotsRotationalMotorController(self.left_front_steer_motor_srv, self.ros)
-        self.right_front_steer_motor = WebotsRotationalMotorController(self.right_front_steer_motor_srv, self.ros)
-        self.left_rear_motor = WebotsRotationalMotorController(self.left_rear_motor_srv, self.ros)
-        self.right_rear_motor = WebotsRotationalMotorController(self.right_rear_motor_srv, self.ros)
         self.ros.register_service(self.get_time_srv, get_float)
 
     def get_time(self):
         response = self.ros.call_service(self.get_time_srv, get_floatRequest(0))
         return response.value if response is not None else None
-    
-    def call_services(self, phi_l, phi_r, w_rear):
-        self.left_front_steer_motor.set_position(phi_l)
-        self.right_front_steer_motor.set_position(phi_r)
-        self.left_rear_motor.set_velocity(w_rear)
-        self.right_rear_motor.set_velocity(w_rear)
     
     def calc_and_output_odom(self, v, R, dt):
         dist = v * dt
@@ -126,51 +113,92 @@ class WebotsROSAckermannActuator(Node):
         current_time = self.get_time()
         last_time = current_time
         while not self.ros.is_shutdown():
-            # 检查接口
-            if 'command' not in self.io.keys():
-                time.sleep(0.1)
-                continue
-
-            # 接收指令
-            if not self.io['command'].poll():
-                continue
-            command = self.io['command'].read()
-            if not (isinstance(command, tuple) or isinstance(command, list)):
-                continue
-
-            # 计算时间
+            # 计算里程计 (不受 command 影响, 有无 command 都会计算里程计)
             current_time = self.get_time()
             if current_time is None or last_time is None:
                 last_time = current_time
                 continue
             dt = current_time - last_time
             last_time = current_time
-            
-            # 计算执行器指令
-            if command[0] == 'vw':
-                v, w = command[1], command[2]
-                sgn = np.sign(v + 1e-3) * np.sign(w + 1e-3)
-                R_min = sgn * self.R_min_abs
-                R = float('inf') if abs(w) < 1e-3 else v / w
-            elif command[0] == 'vphi':
-                v, phi = command[1], command[2]
-                sgn = np.sign(v + 1e-3) * np.sign(phi + 1e-3)
-                R_min = sgn * self.R_min_abs
-                R = float('inf') if abs(phi) < 1e-3 else self.d / np.tan(phi)
-            
-            if abs(R) < abs(R_min): # 无法实现该转角，使用最大转角
-                R = R_min
-            
-            phi_l = np.arctan(self.d / (R + self.l / 2))
-            phi_r = np.arctan(self.d / (R - self.l / 2))
-            w_rear = v / self.r
 
-            # p = mp.Process(target = self.call_services, args = (phi_l, phi_r, w_rear), daemon = True)
-            # p.start()
-            self.call_services(phi_l, phi_r, w_rear)
-
-            # 计算里程计
             if 'odom' in self.io.keys():
-                # mp.Process(target = self.calc_and_output_odom, args = (v, R, dt), daemon = True).start()
-                self.calc_and_output_odom(v, R, dt)
+                self.calc_and_output_odom(self.v_last, self.R_last, dt)
+            
+            # 检查接口
+            if 'command' in self.io.keys():
+                # 接收指令
+                if not self.io['command'].poll():
+                    continue
+                command = self.io['command'].read()
+                if not (isinstance(command, tuple) or isinstance(command, list)):
+                    continue
+                
+                # 计算执行器指令
+                if command[0] == 'vw':
+                    v, w = command[1], command[2]
+                    sgn = np.sign(v + 1e-3) * np.sign(w + 1e-3)
+                    R_min = sgn * self.R_min_abs
+                    R = float('inf') if abs(w) < 1e-3 else v / w
+                elif command[0] == 'vphi':
+                    v, phi = command[1], command[2]
+                    sgn = np.sign(v + 1e-3) * np.sign(phi + 1e-3)
+                    R_min = sgn * self.R_min_abs
+                    R = float('inf') if abs(phi) < 1e-3 else self.d / np.tan(phi)
+                
+                if abs(R) < abs(R_min): # 无法实现该转角，使用最大转角
+                    R = R_min
+                
+                phi_l = np.arctan(self.d / (R + self.l / 2))
+                phi_r = np.arctan(self.d / (R - self.l / 2))
+                w_rear = v / self.r
+
+                self.v_last = v
+                self.R_last = R
+
+                # 输出参数
+                if 'param' in self.io.keys():
+                    self.io['param'].write([phi_l, phi_r, w_rear])
+
+
+"""
+    `param`, input (any)
+        format:
+            [phi_l, phi_r, w_rear]
+"""
+class WebotsROSAckermannActuatorCaller(Node):
+    def __init__(self, name,
+        left_front_steer_motor_srv,
+        right_front_steer_motor_srv,
+        left_rear_motor_srv,
+        right_rear_motor_srv
+    ):
+        super().__init__(name)
+        
+        self.left_front_steer_motor_srv = left_front_steer_motor_srv
+        self.right_front_steer_motor_srv = right_front_steer_motor_srv
+        self.left_rear_motor_srv = left_rear_motor_srv
+        self.right_rear_motor_srv = right_rear_motor_srv
+
+        self.ros = ROSContext(self.name)
+        self.left_front_steer_motor = WebotsRotationalMotorController(self.left_front_steer_motor_srv, self.ros)
+        self.right_front_steer_motor = WebotsRotationalMotorController(self.right_front_steer_motor_srv, self.ros)
+        self.left_rear_motor = WebotsRotationalMotorController(self.left_rear_motor_srv, self.ros)
+        self.right_rear_motor = WebotsRotationalMotorController(self.right_rear_motor_srv, self.ros)
+    
+    def run(self):
+        self.ros.init_node(anonymous = False) # 不可在 __init__ 中调用，否则会导致和 main 中的 init_node 冲突
+
+        while not self.ros.is_shutdown():
+            if 'param' not in self.io.keys():
+                time.sleep(0.1)
+                continue
+
+            if not self.io['param'].poll():
+                continue
+            phi_l, phi_r, w_rear = self.io['param'].read()
+
+            self.left_front_steer_motor.set_position(phi_l)
+            self.right_front_steer_motor.set_position(phi_r)
+            self.left_rear_motor.set_velocity(w_rear)
+            self.right_rear_motor.set_velocity(w_rear)
 